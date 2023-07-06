b9d52d777345a3e1c193411b5837fe53c2fc5368
jantgg <smosh360@hotmail.com>
2023-07-06 12:12:12 +0000

    hola

--- src/front/js/component/services.js
+++ src/front/js/component/services.js
@@ -9,7 +9,7 @@ import me from '../images/pexels-atahan-demir-16166521.jpg';
 const Services = ({ data, groupSize }) => {
   const Navigate = useNavigate();
   const { store, actions } = useContext(Context);
-  const isNotMobile = window.innerWidth >= 1400;
+  const isNotMobile = window.innerWidth >= 700;
 
   return (
     <>
@@ -21,7 +21,7 @@ const Services = ({ data, groupSize }) => {
           </div>
           <div className="col-6 d-flex white ps-3">
             <div className="col-xxl-10 col-12 ">
-              <h1 className="name-size">Hi,<br></br>It's Juan Antonio Gomez</h1>
+              <h1 className="service-size">Hi,<br></br>It's Juan Antonio Gomez</h1>
               <p className="text-size h-100"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a consequat ipsum. Maecenas vel tincidunt sem.
                 Nam lacinia, arcu sit amet lacinia porta, odio ipsum rhoncus enim, id vestibulum tellus urna eget arcu.
                 Integer viverra risus tristique mi condimentum sodales. Sed tristique tellus nec neque vehicula, nec sodales risus porttitor.
@@ -37,20 +37,20 @@ const Services = ({ data, groupSize }) => {
         </div>
       </div>) : (<div className="d-flex flex-column">
         <div className="col-11 serviceh mx-auto service-top d-flex">
-          <div className="col-6 d-flex">
-            <div className="col-12 h100 pe-3 image-container"> <img src={me} alt="Background Image" className="img-style" /></div>
-          </div>
-          <div className="col-6 d-flex white ps-3">
-            <div className="col-12 ">
-              <h1 className="name-size">Hi,<br></br>It's Juan Antonio Gomez</h1>
-              <p className="text-size h-100"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a consequat ipsum. Maecenas vel tincidunt sem.
-                Nam lacinia, arcu sit amet lacinia porta, odio ipsum rhoncus enim, id vestibulum tellus urna eget arcu.
-                Integer viverra risus tristique mi condimentum sodales. Sed tristique tellus nec neque vehicula, nec sodales risus porttitor.
-                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                Pellentesque quis turpis at diam vulputate lacinia at egestas turpis.
-                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
-              </p>
-              <div></div>
+          <div className="col-12 d-flex white mx-auto  p-2" style={{ border: "2px solid white", borderRadius: "2" }}>
+            <div className="col-12" style={{ backgroundImage: `url(${me})`, backgroundSize: 'cover' }}>
+              <div className="image-with-gradient px-5 pt-5 col-12"> <h1 className="name-size">Hi,<br></br>It's Juan Antonio Gomez</h1>
+                <p className="text-size h-100  " style={{ textAlign: 'justify' }}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a consequat ipsum. Maecenas vel tincidunt sem.
+                  Nam lacinia, arcu sit amet lacinia porta, odio ipsum rhoncus enim, id vestibulum tellus urna eget arcu.
+                  Integer viverra risus tristique mi condimentum sodales. Sed tristique tellus nec neque vehicula, nec sodales risus porttitor.
+                  Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                  Pellentesque quis turpis at diam vulputate lacinia at egestas turpis.
+                  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+                </p></div>
+
+              <div>
+
+              </div>
             </div>
 
           </div>
